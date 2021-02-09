358c917f4743c816a814cf84caed2834ee21f0a0
BELGUESMI-Henda <77270487+BELGUESMI-Henda@users.noreply.github.com>
2021-02-09 15:26:41 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "Belguesmi Henda",
+      bio: "Née à Kalaa Khesba ",
+      imgSrc: "./imageProfile",
+      profession: "Comptable"
+    },
+    show: true
+  };
+
+  handleShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show
+    });
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.show && (
+          <>
+            <h1>{this.state.Person.fullName}</h1>
+            <h1>{this.state.Person.bio}</h1>
+            <img src={this.state.Person.imgSrc} alt="Henda"></img>
+            <h1>{this.state.Person.profession}</h1>
+            <br></br>
+          </>
+        )}
+
+        <button onClick={this.handleShowPerson}>click here</button>
+      </>
+    );
+  }
+}
+
+export default App;
